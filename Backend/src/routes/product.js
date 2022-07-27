f5ae399d76c4addd647a76498390b3443ec2c924
Phạm Văn Mẫn <101694980+Manpvph13976@users.noreply.github.com>
2022-07-27 14:45:15 +0700

    Delete Backend/src/routes directory

--- Backend/src/routes/product.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { list, read } from "../controllers/product";
-
-const router = express.Router();
-
-router.get("/products", list);
-router.get("/product/:id", read);
-
-export default router;
